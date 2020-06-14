7182fb18276bebfb605dc0ed8d4daa9a634fbc4b
Doğukan Ali Kutlu <39926739+curlygecko@users.noreply.github.com>
2020-06-14 20:11:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
+
 let colors = ["red", "blue", "green"];
 
 function changeColor(){
     document.getElementById("body").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
     document.getElementById("main").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
-    
+    var song = new Audio();
+    song.src = 'epicMusic.mp3';
+    song.play();
 };
 
 
