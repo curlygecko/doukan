f23b67c1dc9a44f4f469027172ab595683e92bae
Doğukan Ali Kutlu <39926739+curlygecko@users.noreply.github.com>
2020-06-14 17:12:45 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let colors = ["red", "blue", "green"];
 
 function changeColor(){
     document.getElementById("body").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+    document.getElementById("main").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+    
 };
 
 
